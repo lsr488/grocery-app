d0d8c10a8176e3f6d5e53c23627590ac2ff59a0a
unknown <lsr488@gmail.com>
2022-05-20 07:00:35 -0400

    Manually specify mongodb collection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try {
-		const connection = await mongoose.connect(process.env.MONGO_URI);
+		const connection = await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME });
 		console.log(`MonogoDB Connected: ${connection.connection.host}`);
 	} catch (error) {
 		console.log(`Error: ${error.message}`);
