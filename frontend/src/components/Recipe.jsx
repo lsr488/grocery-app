dad4c663a6b045b4cb36b525baf30562acd312d9
unknown <lsr488@gmail.com>
2022-06-22 09:26:27 -0400

    Initial commit

---
+++ frontend/src/components/Recipe.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+import {FaRegSquare, FaPen, FaCheck, FaTrash} from 'react-icons/fa'
+
+function Recipe() {
+	return (
+		<h2>Recipes</h2>
+	)
+}
+
+export default Recipe
