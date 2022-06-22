adf05f1c5ba7ecb6eec00183a092e81fa6c40109
unknown <lsr488@gmail.com>
2022-06-21 23:48:25 -0400

    Removed unused class name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Router>
-        <div className="container">
+        <div>
           <Routes>
             <Route path='/' element={<Home />} />
           </Routes>
