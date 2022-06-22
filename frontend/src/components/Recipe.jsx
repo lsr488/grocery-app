3bbcdce78c2beb1acbea5d4f87e7ad7ebe113da6
unknown <lsr488@gmail.com>
2022-06-22 09:38:09 -0400

    Get recipe info from db and initial display

--- frontend/src/components/Recipe.jsx
+++ frontend/src/components/Recipe.jsx
@@ -3,8 +3,41 @@ import {useState, useEffect} from 'react'
 import {FaRegSquare, FaPen, FaCheck, FaTrash} from 'react-icons/fa'
 
 function Recipe() {
+	const [recipes, setRecipes] = useState([{
+		name: '',
+		notes: '',
+		url: ''
+	}])
+
+	useEffect(() => {
+				getRecipes()
+		}, [])
+
+		// get items from database and setItems based on response
+	const getRecipes = async () => {
+		await axios.get('/api/recipes')
+			.then((response) => {
+				setRecipes(response.data)
+			})
+			.catch((error) => console.log('Error', error))
+	}
+
+	console.log(recipes)
+
+
 	return (
-		<h2>Recipes</h2>
+		<div>
+			<h2>Recipes</h2>
+			<ul>
+				{recipes.map(recipe => (
+					<li>{recipe.name}
+						<li>{recipe.url}</li>
+						<li>{recipe.notes}</li>
+					</li>
+				))}
+			</ul>	
+		</div>
+		
 	)
 }
 
