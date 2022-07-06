9917828d1fae43d66c8f07af7ea5d602cd36c611
Lisa <lsr488@gmail.com>
2022-07-06 06:46:22 -0400

    Delete Recipe

--- frontend/src/components/Recipe.jsx
+++ frontend/src/components/Recipe.jsx
@@ -22,19 +22,33 @@ function Recipe() {
 			.catch((error) => console.log('Error', error))
 	}
 
+	const deleteRecipe = async (id) => {
+		const updatedRecipes = recipes.filter((recipe) => recipe._id !== id)
+		await axios.delete(`/api/recipes/${id}`)
+		setRecipes(updatedRecipes)
+	}
+
 	return (
 		<>
 			<h2 className="heading">Recipes</h2>
 			<ul>
 				{recipes.map((recipe) => (
-					<li className="element-container" key={recipe._id}><FaSquare />
+					<li className="element-container" key={recipe._id}>
+						<span className="element-line-decoration"><FaSquare /></span>
+
 						{recipe.url ? 
 							<span className="element-item-name" id={recipe._id}><a href={recipe.url}>{recipe.name}</a></span> : 
 							<span className="element-item-name" id={recipe._id}>{recipe.name}</span>
 						}
 
-						<span className="element-icon-edit"><FaPen /></span>
-						<span className="element-icon-trash"><FaTrash /></span>
+						{/* <span className="element-icon-edit"><FaPen /></span> */}
+						
+						<span 
+							className="element-icon-trash"
+							onClick={() => deleteRecipe(recipe._id)}
+						>
+							<FaTrash />
+						</span>
 						<span className="element-item-notes">{recipe.notes}</span>
 					</li>
 				))}
