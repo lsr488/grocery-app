bc7c143ffa9ead4265d2522554fc16297d1fa547
Lisa <lsr488@gmail.com>
2022-07-11 09:34:22 -0400

    Remove unused class names

--- frontend/src/components/ItemForm.jsx
+++ frontend/src/components/ItemForm.jsx
@@ -3,23 +3,20 @@ function ItemForm(props) {
 	return (
 			<>
 				<h2>Item Form</h2>
-				<section className="form">
 					<form onSubmit={props.onSubmit}>
-						<div className="form-group">
+						<div>
 							<input 
 								type="text" 
-								className="form-control" 
 								id="name"
 								name="name"
 								placeholder="Enter item name" 
 								required 
 						/>
 						</div>
-						<div className="form-group">
+						<div>
 							<button className="btn">Submit</button>
 						</div>
 					</form>
-				</section>
 			</>
 	)
 }
