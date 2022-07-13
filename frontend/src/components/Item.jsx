51bdb9a8c4215e40f4aca7c761f2e70c591b9a7c
Lisa <lsr488@gmail.com>
2022-07-13 19:24:27 -0400

    Modify Item names with conditionally displayed form

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import {useState, useEffect} from 'react'
 import {FaRegSquare, FaSquare, FaPen, FaCheck, FaTrash} from 'react-icons/fa'
 import ItemForm from './ItemForm'
+import EditItem from './EditItem'
 
 function Item() {
 	const [items, setItems] = useState([{
@@ -23,10 +24,11 @@ function Item() {
 			})
 			.catch((error) => console.log('Error', error))
 	}
-
+	
 	// toggles strikethough on item name when clicked
 	const strikeThrough = async (e) => {
-		const updatedItem = items.filter((item) => item._id === e.target.id)
+		const elementId = e.target.parentNode.id
+		const updatedItem = items.filter((item) => item._id === elementId)
 
 		if(updatedItem[0].status === 'false') {
 			updatedItem[0].status = 'true'
@@ -34,12 +36,10 @@ function Item() {
 			updatedItem[0].status = 'false'
 		}
 
-		// e.target.classList.toggle('checked')
-
-		await axios.put(`api/items/${e.target.id}`, updatedItem[0])
+		await axios.put(`api/items/${elementId}`, updatedItem[0])
 			.catch((error) => console.log('Error', error))
 
-		setItems([...items])
+			setItems([...items])
 	}
 
 	const deleteItem = async (id) => {
@@ -48,10 +48,8 @@ function Item() {
 		setItems(updatedItems)
 	}
 
-	// edit name of item and change CSS background color
+	// changes isEditing status
 	const editItem = async (e) => {
-		// console.log(e)
-		// changeName(e)
 		const updatedItem = items.filter((item) => item._id === e._id)
 
 		updatedItem[0].isEditing = !e.isEditing
@@ -59,15 +57,7 @@ function Item() {
 		await axios.put(`api/items/${e._id}`, updatedItem[0])
 			.catch((error) => console.log('Error', error))
 
-		setItems([...items])
-	}
-
-	const onChange = () => {
-
-	}
-
-	const changeName = (item) => {
-		console.log(item.name)
+			setItems([...items])
 	}
 
 	// add item to db
@@ -84,6 +74,20 @@ function Item() {
 		e.target[0].value = ''
 	}
 
+	const onChange = async (e) => {
+		const value = e.target.value
+		const id = e.target.id
+	
+		const updatedItem = items.filter((item) => item._id === id)
+
+		updatedItem[0].name = value
+
+		await axios.put(`api/items/${id}`, updatedItem[0])
+		.catch((error) => console.log('Error', error))
+
+		setItems([...items])
+	}
+	
 	if(!items) {
 		console.log("no items")
 	}
@@ -95,23 +99,48 @@ function Item() {
 				<h2 className="heading">Items</h2>
 				<ul>
 					{items.map((item) => (
-						<li className="element-container" key={item._id}>
+						<li className="element-container" key={item._id} id={item._id}>
 							<span className="element-line-decoration">
+								{/* change between square styles based on item status */}
 								{item.status === "true" ? <FaSquare /> : <FaRegSquare />}
 							</span>
-							<span
-								// className={`element-item-name ${item.status === 'true' ? 'checked' : null}`}
-								className={`element-item-name ${item.status === 'true' ? 'checked' : null} ${item.isEditing ? 'editing' : null}`}
-								// onClick={strikeThrough}
-								onClick={!item.isEditing ? strikeThrough : null}
-								// contentEditable={item.isEditing}
-								status={item.status}
-								id={item._id}
-								value={item.name}
-							>{item.name}</span>
-							<span onClick={() => editItem(item)} className={`element-icon-save ${item.isEditing ? null : 'hidden'}`}><FaCheck /></span>
-							<span onClick={() => editItem(item)} className={`element-icon-edit ${item.isEditing ? 'hidden' : null}`}><FaPen /></span>
-							<span onClick={() => deleteItem(item._id)} className={`element-icon-trash ${item.isEditing ? 'hidden' : null}`}><FaTrash /></span>
+
+							{/* display editable form if isEditing is true, otherwise display static element */}
+							{item.isEditing ? 
+								<EditItem item={item} onChange={onChange} /> : 
+								<span
+									// strikethrough if clicked
+									className={`element-item-name ${item.status === 'true' ? 'checked' : ''} ${item.isEditing ? 'hidden' : ''}`}
+									// strikeThrough if clicked and not editing
+									onClick={!item.isEditing ? strikeThrough : null}							
+									status={item.status}
+									value={item.name}
+								>{item.name}</span>
+							}
+							
+							{/* Save Icon: set isEditing to false; save icon displays if item is being edited */}
+							<span 
+								onClick={() => editItem(item)} 
+								className={`element-icon-save ${item.isEditing ? '' : 'hidden'}`}
+							>
+								<FaCheck />
+							</span>
+
+							{/* Edit Icon: set isEditing to true; edit icon is displayed if item is not being edited */}
+							<span 
+								onClick={() => editItem(item)} 
+								className={`element-icon-edit ${item.isEditing ? 'hidden' : ''}`}
+							>
+								<FaPen />
+							</span>
+
+							{/* Delete Icon: delete icon is displayed if item is not being edited */}
+							<span 
+								onClick={() => deleteItem(item._id)} 
+								className={`element-icon-trash ${item.isEditing ? 'hidden' : null}`}
+							>
+								<FaTrash />
+							</span>
 						</li>
 					))}
 				</ul>
