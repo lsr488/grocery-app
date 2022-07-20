96cf70d8d1d86cb28a82f9136228875339037a90
Lisa <lsr488@gmail.com>
2022-07-20 08:52:41 -0400

    Set loading status and conditionally display items

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react'
 import {FaRegSquare, FaSquare, FaPen, FaCheck, FaTrash} from 'react-icons/fa'
 import ItemForm from './ItemForm'
 import EditItem from './EditItem'
+import Loading from './Loading'
 
 function Item() {
 	const [items, setItems] = useState([{
@@ -11,6 +12,7 @@ function Item() {
 		isEditing: '',
 		_id: ''
 	}])
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		getItems()
@@ -72,11 +74,19 @@ function Item() {
 	const addItem = async (e) => {
 		e.preventDefault()
 
+		setIsLoading(true)
+
 		const item = {name: e.target[0].value}
 
 		await axios.post('/api/items', item)
-			.then((response) => setItems(prevItems => [...prevItems, response.data]))
-			.catch((error) => console.log('Error', error))
+			.then((response) => {
+				setItems(prevItems => [...prevItems, response.data])
+				setIsLoading(false)
+			})
+			.catch((error) => {
+				console.log('Error', error)
+				setIsLoading(false)
+			})
 
 		// sets input field back to empty
 		e.target[0].value = ''
@@ -102,56 +112,65 @@ function Item() {
 
 	return (
 			<>
-				<ItemForm onSubmit={addItem}/>
+				
+
+				<ItemForm onSubmit={addItem} />
 
 				<h2 className="heading">Items</h2>
 				<ul>
-					{items.map((item) => (
-						<li className="element-container" key={item._id} id={item._id}>
-							<span className="element-line-decoration">
-								{/* change between square styles based on item status */}
-								{item.isChecked === true ? <FaSquare /> : <FaRegSquare />}
-							</span>
+					{isLoading === true ? <Loading /> : 
+						<>
+							{items.map((item) => (
+								<li className="element-container" key={item._id} id={item._id}>
+									{/* change between square styles based on item status */}
+									<span className="element-line-decoration">
+										{item.isChecked === true ? <FaSquare /> : <FaRegSquare />}
+									</span>
+		
+									{/* display editable form if isEditing is true, otherwise display static element */}
+									{item.isEditing ? 
+										<EditItem item={item} onChange={onChange} /> : 
+										<span
+											// strikethrough if clicked
+											className={`element-item-name ${item.isChecked === true ? 'checked' : ''} ${item.isEditing ? 'hidden' : ''}`}
+											// strikeThrough if clicked and not editing
+											onClick={!item.isEditing ? strikeThrough : null}							
+											// why do we need to pass in item.isChecked here...?
+											// isChecked={item.isChecked}
+											value={item.name}
+										>{item.name}</span>
+									}
+									
+									{/* Save Icon: set isEditing to false; save icon displays if item is being edited */}
+									<span 
+										onClick={() => editItem(item)} 
+										className={`element-icon-save ${item.isEditing ? '' : 'hidden'}`}
+									>
+										<FaCheck />
+									</span>
+		
+									{/* Edit Icon: set isEditing to true; edit icon is displayed if item is not being edited */}
+									<span 
+										onClick={() => editItem(item)} 
+										className={`element-icon-edit ${item.isEditing ? 'hidden' : ''}`}
+									>
+										<FaPen />
+									</span>
+		
+									{/* Delete Icon: delete icon is displayed if item is not being edited */}
+									<span 
+										onClick={() => deleteItem(item._id)} 
+										className={`element-icon-trash ${item.isEditing ? 'hidden' : null}`}
+									>
+										<FaTrash />
+									</span>
+								</li>
+							))}
+						</>
 
-							{/* display editable form if isEditing is true, otherwise display static element */}
-							{item.isEditing ? 
-								<EditItem item={item} onChange={onChange} /> : 
-								<span
-									// strikethrough if clicked
-									className={`element-item-name ${item.isChecked === true ? 'checked' : ''} ${item.isEditing ? 'hidden' : ''}`}
-									// strikeThrough if clicked and not editing
-									onClick={!item.isEditing ? strikeThrough : null}							
-									// why do we need to pass in item.isChecked here...?
-									// isChecked={item.isChecked}
-									value={item.name}
-								>{item.name}</span>
-							}
-							
-							{/* Save Icon: set isEditing to false; save icon displays if item is being edited */}
-							<span 
-								onClick={() => editItem(item)} 
-								className={`element-icon-save ${item.isEditing ? '' : 'hidden'}`}
-							>
-								<FaCheck />
-							</span>
+					}
 
-							{/* Edit Icon: set isEditing to true; edit icon is displayed if item is not being edited */}
-							<span 
-								onClick={() => editItem(item)} 
-								className={`element-icon-edit ${item.isEditing ? 'hidden' : ''}`}
-							>
-								<FaPen />
-							</span>
 
-							{/* Delete Icon: delete icon is displayed if item is not being edited */}
-							<span 
-								onClick={() => deleteItem(item._id)} 
-								className={`element-icon-trash ${item.isEditing ? 'hidden' : null}`}
-							>
-								<FaTrash />
-							</span>
-						</li>
-					))}
 				</ul>
 		</>
 	)
