a5bc0fbf30a4b6d0c11dffff5e9adf9666d3e15e
Lisa <lsr488@gmail.com>
2022-07-24 09:44:32 -0400

    remove h2

--- frontend/src/components/ItemForm.jsx
+++ frontend/src/components/ItemForm.jsx
@@ -2,21 +2,20 @@ function ItemForm(props) {
 
 	return (
 			<>
-				<h2>Item Form</h2>
-					<form onSubmit={props.onSubmit}>
-						<div>
-							<input 
-								type="text" 
-								id="name"
-								name="name"
-								placeholder="Enter item name"
-								required 
-						/>
-						</div>
-						<div>
-							<button className="btn">Submit</button>
-						</div>
-					</form>
+				<form onSubmit={props.onSubmit}>
+					<div>
+						<input 
+							type="text" 
+							id="name"
+							name="name"
+							placeholder="Enter item name"
+							required 
+					/>
+					</div>
+					<div>
+						<button className="btn">Submit</button>
+					</div>
+				</form>
 			</>
 	)
 }
